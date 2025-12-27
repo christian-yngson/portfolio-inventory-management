ef5316babe44be62a381ad9963f82be131e4d055
Christian Yngson <christianwillyngson@gmail.com>
2025-12-27 19:39:31 +0800

    feat(sidebar): refine sidebar and nav layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
 import Header from "@/components/protected/Header";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Sidebar from "@/components/protected/Sidebar";
+import SidebarProvider from "@/components/protected/SidebarProvider";
+import MainContentContainer from "@/components/protected/MainContentContainer";
 
 interface Props {
   children: ReactNode;
@@ -7,10 +12,15 @@ interface Props {
 
 function ProtectedLayout({ children }: Props) {
   return (
-    <>
-      <Header />
-      <main>{children}</main>
-    </>
+    <SidebarProvider>
+      <Stack direction="row" sx={{ minHeight: "100vh", width: "100%" }}>
+        <Sidebar />
+        <MainContentContainer>
+          <Header />
+          <Box component="main">{children}</Box>
+        </MainContentContainer>
+      </Stack>
+    </SidebarProvider>
   );
 }
 
--- src/components/protected/Header/Header.tsx
+++ src/components/protected/Header/Header.tsx
@@ -1,24 +1,20 @@
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import AvatarButton from "./AvatarButton";
 import SidebarToggle from "./SidebarToggle";
 import HeaderLabel from "./HeaderLabel";
 import HeaderIconButtons from "./HeaderIconButtons";
 
-/* @TODO add test immediate */
 function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <SidebarToggle />
-          <HeaderLabel />
-          <HeaderIconButtons />
-          <AvatarButton />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <SidebarToggle />
+        <HeaderLabel />
+        <HeaderIconButtons />
+        <AvatarButton />
+      </Toolbar>
+    </AppBar>
   );
 }
 
