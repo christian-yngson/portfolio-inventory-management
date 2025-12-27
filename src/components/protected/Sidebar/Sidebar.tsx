9d31e2f87dd7007c672071e8f624602e6f1ba7cc
Christian Yngson <christianwillyngson@gmail.com>
2025-12-27 19:11:45 +0800

    feat(sidebar): add initial sidebar component

---
+++ src/components/protected/Sidebar/Sidebar.tsx
@@ -0,0 +1,47 @@
+import HeaderSection from "./HeaderSection";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import SidebarDrawer from "./SidebarDrawer";
+
+/* @TODO add test immediate */
+function Sidebar() {
+  return (
+    <SidebarDrawer>
+      <HeaderSection />
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </SidebarDrawer>
+  );
+}
+
+export default Sidebar;
