5dd365bbf11f1df5b63ef3ccacf2eff03b2a958f
Christian Yngson <christianwillyngson@gmail.com>
2025-12-28 00:15:25 +0800

    chore: add initial pages for products and add products

---
+++ src/app/(protected)/products/add/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Products | Add",
+  description: "Add a new product page.",
+};
+
+function AddProductsPage() {
+  return <div>Add Product Page</div>;
+}
+
+export default AddProductsPage;
